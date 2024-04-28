d00ad0ab470a47335e08f6f13953679e1ac51a25
Mseher <77393579+Mseher@users.noreply.github.com>
2024-04-28 05:11:24 -0700

    Update map.js

--- src/components/map.js
+++ src/components/map.js
@@ -17,17 +17,15 @@ const GulfOfMexicoMap = () => {
 
   const [showEnergyInfrastructure, setShowEnergyInfrastructure] =
     useState(false);
-  
-    const [showStateData, setShowStateData] =
-    useState(false);
+
+  const [showStateData, setShowStateData] = useState(false);
 
   const [stateLayer, setStateLayer] = useState(null);
   const [map, setMap] = useState(null);
 
   const handleDesalinationCheckboxChange = (checked) => {
-    // If any of the desalination checkboxes are checked, uncheck the energy infrastructure and others checkbox
+    // If any of the desalination checkboxes are checked, uncheck the energy infrastructure checkbox
     setShowEnergyInfrastructure(false);
-    setShowStateData(false);
   };
 
   const handleEnergyInfrastructureCheckbox = (checked) => {
@@ -48,8 +46,6 @@ const GulfOfMexicoMap = () => {
     }
     setShowStateData(checked);
   };
-
-  
 
   useEffect(() => {
     const mapInstance = L.map("map").setView([27.994402, -90.502335], 6);
@@ -154,58 +150,79 @@ const GulfOfMexicoMap = () => {
     function generatePopupContent(projectName, otherFields, source) {
       const popupContent = document.createElement("div");
       popupContent.style.maxWidth = "100%"; // Ensure the popup does not exceed the screen width
+
+      
       popupContent.innerHTML = `
       <div class="popup-header">${projectName}</div>
       <div class="popup-body">
           <table class="details-table">
               ${Object.entries(otherFields)
-                  .reduce((rows, [fieldName, fieldValue], index) => {
-                      if (index % 2 === 0) rows.push([]); // Start a new row every two entries
-                      const contentId = `${fieldName}_content_${Date.now()}_${Math.random().toString(36).substr(2, 9)}`;
-                      const fieldHTML = fieldValue.length > 70 ? `
-                          <span id="${contentId}_short">${fieldValue.substring(0, 50)}...</span>
+                .reduce((rows, [fieldName, fieldValue], index) => {
+                  if (index % 2 === 0 || fieldName==="Latest Updates") rows.push([]); // Start a new row every two entries
+                  const contentId = `${fieldName}_content_${Date.now()}_${Math.random()
+                    .toString(36)
+                    .substr(2, 9)}`;
+                  const fieldHTML =
+                    fieldValue.length > 120
+                      ? `
+                          <span id="${contentId}_short">${fieldValue.substring(
+                          0,
+                          50
+                        )}...</span>
                           <span id="${contentId}_full" class="full-content">${fieldValue}</span>
                           <a href="#" class="read-more-link" data-id="${contentId}">Read More</a>
-                      ` : `${fieldValue}`;
-                      rows[rows.length - 1].push(`<td class="field-name">${fieldName}</td><td class="field-value">${fieldHTML}</td>`);
-                      return rows;
-                  }, [])
-                  .map(row => `<tr>${row.join('')}</tr>`)
-                  .join("")}
+                      `
+                      : `${fieldValue}`;
+                      if (fieldName === "Latest Updates") {
+                        rows[rows.length - 1].push(
+                          `<tr><td  class="field-name">${fieldName}</td><td colspan="3" class="field-value">${fieldHTML}</td></tr>`
+                        );
+                      }
+                      else{
+                        rows[rows.length - 1].push(
+                          `<td class="field-name">${fieldName}</td><td class="field-value">${fieldHTML}</td>`
+                        );
+                      }
+
+                  return rows;
+                }, [])
+                .map((row) => `<tr>${row.join("")}</tr>`)
+                .join("")}
           </table>
       </div>
       
       <div>
+      
   <button onclick="window.open('${source}', '_blank');" class="source-button">Source</button>
 </div>
 
       `;
-  
+
       // Attach event listener for "Read More" links
       popupContent.querySelectorAll(".read-more-link").forEach((link) => {
-          link.addEventListener("click", (e) => {
-              e.preventDefault();
-              const uniqueId = e.target.getAttribute("data-id");
-              const shortContent = document.getElementById(`${uniqueId}_short`);
-              const fullContent = document.getElementById(`${uniqueId}_full`);
-              const expanded = e.target.getAttribute("data-expanded") === "true";
-  
-              if (expanded) {
-                  shortContent.style.display = "inline";
-                  fullContent.style.display = "none";
-                  e.target.textContent = "Read More";
-                  e.target.setAttribute("data-expanded", "false");
-              } else {
-                  shortContent.style.display = "none";
-                  fullContent.style.display = "inline";
-                  e.target.textContent = "Read Less";
-                  e.target.setAttribute("data-expanded", "true");
-              }
-          });
+        link.addEventListener("click", (e) => {
+          e.preventDefault();
+          const uniqueId = e.target.getAttribute("data-id");
+          const shortContent = document.getElementById(`${uniqueId}_short`);
+          const fullContent = document.getElementById(`${uniqueId}_full`);
+          const expanded = e.target.getAttribute("data-expanded") === "true";
+
+          if (expanded) {
+            shortContent.style.display = "inline";
+            fullContent.style.display = "none";
+            e.target.textContent = "Read More";
+            e.target.setAttribute("data-expanded", "false");
+          } else {
+            shortContent.style.display = "none";
+            fullContent.style.display = "inline";
+            e.target.textContent = "Read Less";
+            e.target.setAttribute("data-expanded", "true");
+          }
+        });
       });
-  
+
       return popupContent;
-  }
+    }
 
     // Add markers from JSON data with custom icons
     data.forEach((markerData) => {
@@ -230,7 +247,9 @@ const GulfOfMexicoMap = () => {
               icon: markerIcon,
             }) // Modify this line to use "lat" and "long"
               .addTo(map)
-              .bindPopup(generatePopupContent(projectName, otherFields, source));
+              .bindPopup(
+                generatePopupContent(projectName, otherFields, source)
+              );
             marker.markerData = { lat, long, status, size, ...otherFields }; // Store the entire markerData object as a property of the marker
             markers.push(marker);
           }
@@ -244,7 +263,6 @@ const GulfOfMexicoMap = () => {
     };
   }, [map, showPlanned, showUnderConstruction, showOperational]);
 
-    
   useEffect(() => {
     if (!map) return;
 
@@ -252,44 +270,239 @@ const GulfOfMexicoMap = () => {
     if (showStateData && !stateLayer) {
       const layer = L.geoJson(stateData, {
         onEachFeature: (feature, layer) => {
-
           // Define mouseover event
-      layer.on('mouseover', (e) => {
-        // Change the color of the polygon to brown on hover
-        e.target.setStyle({
-          fillColor: 'brown',
-          color: '#d9534f',
-          weight: 2,
-        });
-      });
-      // Define mouseout event
-      layer.on('mouseout', (e) => {
-        // Reset the color of the polygon when not hovering
-        e.target.setStyle({
-          fillColor: '#90EE90', // Original color
-          color: '#006400',
-          weight: 2,
-        });
-      });
+          layer.on("mouseover", (e) => {
+            // Change the color of the polygon to brown on hover
+            e.target.setStyle({
+              fillColor: "brown",
+              color: "#d9534f",
+              weight: 2,
+            });
+          });
+          // Define mouseout event
+          layer.on("mouseout", (e) => {
+            // Reset the color of the polygon when not hovering
+            e.target.setStyle({
+              fillColor: "#90EE90", // Original color
+              color: "#006400",
+              weight: 2,
+            });
+          });
 
           if (feature.properties && feature.properties.name) {
-            layer.bindPopup(feature.properties.name);
+            layer.bindPopup(generatePopupContent(feature).innerHTML);
           }
         },
         style: {
           // Define default styles for your polygons or based on feature properties
-          color: '#006400',
+          color: "#006400",
           weight: 2,
           opacity: 1,
-          fillColor: '#90EE90',
-          fillOpacity: 0.7
-        }
+          fillColor: "#90EE90",
+          fillOpacity: 0.7,
+        },
       }).addTo(map);
       setStateLayer(layer);
     } else if (!showStateData && stateLayer) {
       // Remove GeoJSON layer if it exists and the checkbox is not checked
       map.removeLayer(stateLayer);
       setStateLayer(null);
+    }
+    function generatePopupContent(feature) {
+      const properties = feature.properties;
+
+      // Function to safely retrieve nested properties
+      const getNestedValue = (object, keys) =>
+        keys.reduce((o, k) => (o || {})[k], object);
+
+      // Create a function to format the number with commas for thousands
+      const formatNumber = (num) => num?.toLocaleString() || num;
+
+      const popupContent = document.createElement("div");
+      popupContent.style.maxWidth = "100%"; // Ensure the popup does not exceed the screen width
+      popupContent.innerHTML = `
+      <div class="state-popup-content">
+      <div class="popup-header" style=" width: 100%;">${properties.name}</div>
+      <br>
+      <div><strong>Population:</strong> ${formatNumber(
+        properties.Population
+      )}</div>
+      <div class="popup-body">
+        <table class="details-table" style="width: 100%; ">
+        <tr>
+            <!-- Electricity Data Column -->
+            <td style="vertical-align: top; width: 50%; padding-left: 0; padding-right: 5px;">
+                <table  >
+                    <tr><th colspan="2" style="padding-left: 0; padding-right: 5px;">Electricity Data</th></tr>
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">Production</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityData",
+                        "Production",
+                        "MWh",
+                      ])
+                    )} MWh <br>${formatNumber(
+        getNestedValue(properties, [
+          "ElectricityData",
+          "Production",
+          "MWh per capita",
+        ])
+      )} MWh per capita</td></tr>
+                    
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">Consumption</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityData",
+                        "Consumption",
+                        "MWh",
+                      ])
+                    )} MWh <br>${formatNumber(
+        getNestedValue(properties, [
+          "ElectricityData",
+          "Consumption",
+          "MWh per capita",
+        ])
+      )} MWh per capita</td></tr>
+                    
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">Renewable</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityData",
+                        "Production from Renewable",
+                        "MWh",
+                      ])
+                    )} MWh <br>${formatNumber(
+        getNestedValue(properties, [
+          "ElectricityData",
+          "Production from Renewable",
+          "MWh per capita",
+        ])
+      )} MWh per capita</td></tr>
+                    
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">Non-Renewable</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityData",
+                        "Production from Non-Renewable",
+                        "MWh",
+                      ])
+                    )} MWh <br>${formatNumber(
+        getNestedValue(properties, [
+          "ElectricityData",
+          "Production from Non-Renewable",
+          "MWh per capita",
+        ])
+      )} MWh per capita</td></tr>
+                    
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">CO2 Emissions</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityData",
+                        "CO2 Emissions from Consumption",
+                        "kg",
+                      ])
+                    )} kg <br>${formatNumber(
+        getNestedValue(properties, [
+          "ElectricityData",
+          "CO2 Emissions from Consumption",
+          "kg per capita",
+        ])
+      )} kg per capita</td></tr>
+                    
+                    <tr><th colspan="2">Electricity Rate and Bills</th></tr>
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">Residential</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityRateandBills",
+                        "Residential",
+                        "centskWh",
+                      ])
+                    )} cents/kWh </td></tr>
+                    <tr><td style="width: 50%; padding-left: 0; padding-right: 5px;">Ave. monthly Bill</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityRateandBills",
+                        "Residential",
+                        "Ave. monthly Bill",
+                      ])
+                    )}</td></tr>
+                    
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">Commercial</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityRateandBills",
+                        "Commercial",
+                        "centskWh",
+                      ])
+                    )} cents/kWh </td></tr>
+                    <tr><td style="width: 50%; padding-left: 0; padding-right: 5px;">Ave. monthly Bill</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityRateandBills",
+                        "Commercial",
+                        "Ave. monthly Bill",
+                      ])
+                    )}</td></tr>
+                    
+                    <tr><td style="width: 40%; padding-left: 0; padding-right: 5px;">Industrial</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityRateandBills",
+                        "Industrial",
+                        "centskWh",
+                      ])
+                    )} cents/kWh </td></tr>
+                    <tr><td style="width: 50%; padding-left: 0; padding-right: 5px;">Ave. monthly Bill</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "ElectricityRateandBills",
+                        "Industrial",
+                        "Ave. monthly Bill",
+                      ])
+                    )}</td></tr>
+                  </table>
+            </td>
+            <!-- Water Data Column -->
+            <td style="vertical-align: top; width: 50%; margin-left:2px">
+                <table >
+                    <tr><th colspan="2">Water Data</th></tr>
+                    <tr><td>Current Demand</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "WaterData",
+                        "Current Water Demand",
+                        "acre-feet/year",
+                      ])
+                    )} acre-feet/year</td></tr>
+                    <tr><td>In-State Supply</td><td>${formatNumber(
+                      getNestedValue(properties, [
+                        "WaterData",
+                        "In-State Supply ",
+                        "acre-feet/year",
+                      ])
+                    )} acre-feet/year</td></tr>
+                    
+                    <tr><th colspan="2">Water Usage by Type</th></tr>
+                    <tr><td>Municipal</td><td>${
+                      properties.WaterUsagebyType.Municipal
+                    }</td></tr>
+                    <tr><td>Irrigation</td><td>${
+                      properties.WaterUsagebyType.Irrigation
+                    }</td></tr>
+                    <tr><td>Livestock</td><td>${
+                      properties.WaterUsagebyType.Livestock
+                    }</td></tr>
+                    <tr><td>Industry</td><td>${
+                      properties.WaterUsagebyType.Industry
+                    }</td></tr>
+                    <tr><td>Electric Power</td><td>${
+                      properties.WaterUsagebyType.ElectricPower
+                    }</td></tr>
+
+
+                    <tr><th style="width: 50%;">Average Residential Monthly Water Bill</th><td>${
+                      properties.AverageResidentialMonthlyWaterBill
+                    }</td></tr>
+                    
+                </table>
+            </td>
+        </tr>
+    </table>
+        
+      </div>
+        
+      </div>
+    `;
+
+      return popupContent;
     }
 
     return () => {
@@ -299,8 +512,6 @@ const GulfOfMexicoMap = () => {
       }
     };
   }, [map, showStateData, stateLayer]);
-
-
 
   return (
     <Row>
@@ -368,9 +579,7 @@ const GulfOfMexicoMap = () => {
                 type="checkbox"
                 label="State Data"
                 checked={showStateData}
-                onChange={(e) =>
-                  handleStateDataCheckbox(e.target.checked)
-                }
+                onChange={(e) => handleStateDataCheckbox(e.target.checked)}
               />
             </Form>
           </div>
